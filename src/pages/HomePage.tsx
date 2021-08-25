3f52b8b23ff34913eb923f4807e3de70b906599d
VuHoang <vunh1.vbi@vietinbank.vn>
2021-08-25 14:49:09 +0700

    config

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+`;
 
 const HomePage = () => {
-    return(
-        <div>Hello World!!</div>
-    )
-}
+  return (
+    <div>
+      <Image
+        src="https://api.evbi.vn/images/moto_files/VBI-bao-hiem-mo-to.png"
+        alt="Bảo hiểm TNDS mô tô xe máy"
+      />
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
