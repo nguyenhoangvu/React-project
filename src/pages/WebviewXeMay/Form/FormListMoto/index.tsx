4a4179efc7a7f91f2734f42dbb644325a7f6535d
VuHoang <vunh1.vbi@vietinbank.vn>
2021-09-17 11:08:04 +0700

    update all back next form

--- src/pages/WebviewXeMay/Form/FormListMoto/index.tsx
+++ src/pages/WebviewXeMay/Form/FormListMoto/index.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
-const TextInput = () => {
-  return <div>Hello World!!</div>;
+import DirectButton from "../../../DirectButton";
+import TextInput from "../../../../components/TextInput";
+import SelectDropdown from "../../../../components/SelectDropdown";
+
+type Props = {
+  handleButtonClick: (clicked: string) => void;
+  pageCallback: string;
 };
 
-export default TextInput;
+const FormListMoto: React.FC<Props> = ({ handleButtonClick, pageCallback }) => {
+  const [buttonClick, setButtonClick] = useState("");
+
+  const handleDisplayForm = (buttonClicked: string) => {
+    setButtonClick(buttonClicked);
+  };
+
+  useEffect(() => {
+    handleButtonClick ? handleButtonClick(buttonClick) : {};
+  }, [buttonClick]);
+
+  useEffect(() => {
+    setButtonClick(pageCallback);
+  }, [pageCallback]);
+  return (
+    <div>
+      list moto!!
+      <DirectButton
+        handleButtonClick={handleDisplayForm}
+        buttonCallback={buttonClick}
+      />
+    </div>
+  );
+};
+
+export default FormListMoto;
--- src/pages/WebviewXeMay/Form/index.tsx
+++ src/pages/WebviewXeMay/Form/index.tsx
@@ -3,6 +3,9 @@ import styled from "styled-components";
 
 import FormUserInfo from "./FormUserInfo";
 import FormMotoInfo from "./FormMotoInfo";
+import FormTNDS from "./FormTNDS";
+import FormListMoto from "./FormListMoto";
+import FormSummary from "./FormSummary";
 
 interface IFormUserInfo {
   hideForm: boolean;
@@ -20,6 +23,30 @@ const FormMotoInfoWrapper = styled.div<IFormMotoInfo>`
   display: ${(props) => (props.showForm === true ? "block" : "none")};
 `;
 
+interface IFormTNDS {
+  showForm: boolean;
+}
+
+const FormTNDSWrapper = styled.div<IFormTNDS>`
+  display: ${(props) => (props.showForm === true ? "block" : "none")};
+`;
+
+interface IFormListMoto {
+  showForm: boolean;
+}
+
+const FormListMotoWrapper = styled.div<IFormListMoto>`
+  display: ${(props) => (props.showForm === true ? "block" : "none")};
+`;
+
+interface IFormSummary {
+  showForm: boolean;
+}
+
+const FormSummaryWrapper = styled.div<IFormSummary>`
+  display: ${(props) => (props.showForm === true ? "block" : "none")};
+`;
+
 type Props = {
   handleDirectButtonClick: (clicked: string) => void;
   pageCallback: boolean;
@@ -34,9 +61,17 @@ const FormXeMay: React.FC<Props> = ({
 
   const [buttonFormMotoInfoClicked, setButtonFormMotoInfoClicked] =
     useState("");
+  const [buttonFormTNDSClicked, setButtonFormTNDSClicked] = useState("");
+  const [buttonFormListMotoClicked, setButtonFormListMotoClicked] =
+    useState("");
+  const [buttonFormSummaryClicked, setButtonFormSummaryClicked] = useState("");
 
   const [showFormMotoInfo, setShowFormMotoInfo] = useState(false);
+  const [showFormTNDS, setShowFormTNDS] = useState(false);
+  const [showFormListMoto, setShowFormListMoto] = useState(false);
+  const [showFormSummary, setShowFormSummary] = useState(false);
 
+  // show hide form user info
   const handleButtonClick = (buttonClicked: string) => {
     setButtonFormUserInfoClicked(buttonClicked);
     if (buttonClicked === "next") {
@@ -55,12 +90,61 @@ const FormXeMay: React.FC<Props> = ({
     if (pageCallback === true) setButtonFormUserInfoClicked("");
   }, [pageCallback]);
 
+  // show hide from moto info
   const handleDisplayFormMotoInfo = (buttonClicked: string) => {
     if (buttonClicked) {
       setShowFormMotoInfo(false);
       setButtonFormMotoInfoClicked(buttonClicked);
       if (buttonClicked === "back") {
         setButtonFormUserInfoClicked("");
+      } else if (buttonClicked === "next") {
+        setShowFormTNDS(true);
+        setButtonFormTNDSClicked("");
+      }
+    }
+  };
+
+  // show hide from TNDS
+  const handleDisplayFormTNDS = (buttonClicked: string) => {
+    if (buttonClicked) {
+      setShowFormTNDS(false);
+      setButtonFormTNDSClicked(buttonClicked);
+      if (buttonClicked === "back") {
+        setButtonFormMotoInfoClicked("");
+        setShowFormMotoInfo(true);
+      } else if (buttonClicked === "next") {
+        setShowFormListMoto(true);
+        setButtonFormListMotoClicked("");
+      }
+    }
+  };
+
+  // show hide from ListMoto
+  const handleDisplayFormListMoto = (buttonClicked: string) => {
+    if (buttonClicked) {
+      setShowFormListMoto(false);
+      setButtonFormListMotoClicked(buttonClicked);
+      if (buttonClicked === "back") {
+        setButtonFormTNDSClicked("");
+        setShowFormTNDS(true);
+      } else if (buttonClicked === "next") {
+        setShowFormSummary(true);
+        setButtonFormSummaryClicked("");
+      }
+    }
+  };
+
+  // show hide from Summary
+  const handleDisplayFormSummary = (buttonClicked: string) => {
+    if (buttonClicked) {
+      setShowFormSummary(false);
+      setButtonFormSummaryClicked(buttonClicked);
+      if (buttonClicked === "back") {
+        setButtonFormListMotoClicked("");
+        setShowFormListMoto(true);
+      } else if (buttonClicked === "next") {
+        // setShowFormTNDS(true);
+        // setButtonFormTNDSClicked("");
       }
     }
   };
@@ -83,6 +167,24 @@ const FormXeMay: React.FC<Props> = ({
           pageCallback={buttonFormMotoInfoClicked}
         />
       </FormMotoInfoWrapper>
+      <FormTNDSWrapper showForm={showFormTNDS}>
+        <FormTNDS
+          handleButtonClick={handleDisplayFormTNDS}
+          pageCallback={buttonFormTNDSClicked}
+        />
+      </FormTNDSWrapper>
+      <FormListMotoWrapper showForm={showFormListMoto}>
+        <FormListMoto
+          handleButtonClick={handleDisplayFormListMoto}
+          pageCallback={buttonFormListMotoClicked}
+        />
+      </FormListMotoWrapper>
+      <FormSummaryWrapper showForm={showFormSummary}>
+        <FormSummary
+          handleButtonClick={handleDisplayFormSummary}
+          pageCallback={buttonFormSummaryClicked}
+        />
+      </FormSummaryWrapper>
     </form>
   );
 };
