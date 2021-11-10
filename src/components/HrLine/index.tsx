eaf80a3abd4f561ce33b6d184b71a1e73dd15832
VuHoang <vunh1.vbi@vietinbank.vn>
2021-11-10 10:59:37 +0700

    update home

--- src/components/HrLine/index.tsx
+++ src/components/HrLine/index.tsx
@@ -1,8 +1,23 @@
 import React from "react";
-import "./index.scss";
+import styled from "styled-components";
 
-const HrLine = () => {
-  return <hr className="line"></hr>;
+type Props = {
+  isMargin?: boolean;
+};
+
+interface IStyledHr {
+  isMarginTop?: boolean;
+}
+
+const Hr = styled.hr<IStyledHr>`
+  border: 0.0625rem solid #e0e0e0;
+  border-bottom: 0px;
+  height: 0px;
+  margin-top: ${(props) => (props.isMarginTop === true ? "0" : "1rem")};
+`;
+
+const HrLine: React.FC<Props> = ({ isMargin }) => {
+  return <Hr isMarginTop={isMargin} />;
 };
 
 export default HrLine;
--- src/pages/IHome/index.tsx
+++ src/pages/IHome/index.tsx
@@ -3,12 +3,19 @@ import styled from "styled-components";
 import { Routes, Route, Link } from "react-router-dom";
 import logo from "../../images/bao-hiem.png";
 import iconMoto from "../../images/moto.svg";
+import iconOto from "../../images/dgrr_xe.svg";
+import iconHealth from "../../images/hospital.svg";
 import iconChevon from "../../images/ic_chevron_right.svg";
 import data from "../../json/partner-info.json";
 import HrLine from "../../components/HrLine";
 import Webview from "../Webview";
 
-const LogoContainer = styled.div`
+interface ILogoContainer {
+  isShow: boolean;
+}
+
+const LogoContainer = styled.div<ILogoContainer>`
+  display: ${(props) => (props.isShow === true ? "block" : "none")};
   position: relative;
   box-shadow: none;
   height: 85px;
@@ -30,13 +37,13 @@ const LinkContentContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 10px 0;
+  padding: 15px 0;
 `;
 
 const LinkContent = styled.div`
   color: black;
   line-height: 34px;
-  width: 70%;
+  width: 55%;
 `;
 
 const IconStyle = styled.i`
@@ -44,30 +51,79 @@ const IconStyle = styled.i`
 `;
 
 const IHome = () => {
+  const [show, setShow] = useState(true);
+  const [listIcon, setListIcon] = useState<any>([]);
   const listNV = data.list_nv;
+
+  useEffect(() => {
+    let arr: any[] = [];
+    listNV.forEach((element) => {
+      switch (element.key) {
+        case "XC.1.1":
+          let objMoto = {
+            key: element.key,
+            icon: iconMoto,
+          };
+          arr.push(objMoto);
+          break;
+        case "XE":
+          let objCar = {
+            key: element.key,
+            icon: iconOto,
+          };
+          arr.push(objCar);
+          break;
+        case "CN.6":
+          let objHealth = {
+            key: element.key,
+            icon: iconHealth,
+          };
+          arr.push(objHealth);
+          break;
+        default:
+          break;
+      }
+    });
+    setListIcon([...arr]);
+  }, []);
+
+  const handleClickRoute = () => {
+    setShow(false);
+  };
+
+  const RenderLink = () => {
+    const ListLink = listNV.map((o) => (
+      <Link key={o.key} to={o.route} onClick={handleClickRoute}>
+        <>
+          <LinkContentContainer>
+            <IconStyle>
+              {listIcon.map((key: any, index: any) => (
+                <React.Fragment key={index}>
+                  {key.key == o.key && <Image src={key.icon} alt="icon" />}
+                </React.Fragment>
+              ))}
+            </IconStyle>
+            <LinkContent>{o.value}</LinkContent>
+            <IconStyle>
+              <Image src={iconChevon} alt="icon" />
+            </IconStyle>
+          </LinkContentContainer>
+          <HrLine isMargin={true} />
+        </>
+      </Link>
+    ));
+    return <>{ListLink}</>;
+  };
 
   return (
     <div id="home">
-      <LogoContainer>
+      <LogoContainer isShow={show}>
         <div style={{ textAlign: "center" }}>
           <img src={logo} alt="logo" width="300px" />
         </div>
       </LogoContainer>
       <NavContainer>
-        <Link to="">
-          <>
-            <LinkContentContainer>
-              <IconStyle>
-                <Image src={iconMoto} alt="icon moto" />
-              </IconStyle>
-              <LinkContent>Bảo hiểm xe máy</LinkContent>
-              <i>
-                <Image src={iconChevon} alt="icon" />
-              </i>
-            </LinkContentContainer>
-            <HrLine />
-          </>
-        </Link>
+        <RenderLink />
       </NavContainer>
     </div>
   );
